5c345fcf049451d139ab19ca9dec9efb0af6efb7
Robert Jones <robertdante@outlook.com>
2024-09-11 17:46:32 -0400

    multiples by user input

--- Wk4_ Assignments/Jones_Robert_Wk4_program.py
+++ Wk4_ Assignments/Jones_Robert_Wk4_program.py
@@ -10,6 +10,24 @@ start = 1
 end = 11
 user = int(input('Enter number from 1-11: \n'))
 
+# validate user input
+while user >= 1 and user <= 11:
+    # print results
+    print(f'Printing out multiples of {user} \n')
+    for count in range(start, end +1):
+        new = count * user
+        print(f"{user} x {count} = {new}")
+    # check if user wants to end program
+    userQuit = input('Would you like to quit the program (yes/no): \n')
+    if userQuit == 'yes':
+        break
+    # checking validation so while loop can repeat
+    elif userQuit == 'no':
+        user = int(input('Enter number from 1-11: \n'))
+        continue
+
+
+
 
 
 
