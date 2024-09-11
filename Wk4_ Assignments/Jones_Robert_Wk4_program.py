d6e9495d09c6fab7ae2bf7b765a33e7104b85886
Robert Jones <robertdante@outlook.com>
2024-09-11 17:42:13 -0400

    Calculator Project

---
+++ Wk4_ Assignments/Jones_Robert_Wk4_program.py
@@ -0,0 +1,18 @@
+'''
+Name: Robert Jones
+Date: 9/11/2024
+Purpose: This program is intended to provide calculations for multiples of 7 based on the user's input
+Version: 1.0
+'''
+
+
+
+
+
+
+
+
+
+
+
+
