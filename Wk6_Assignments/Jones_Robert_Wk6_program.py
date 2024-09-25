3ea280a1bec0ff419111aea2f42708dcae5dac09
Robert Jones <robertdante@outlook.com>
2024-09-24 21:02:06 -0400

    get user input

--- Wk6_Assignments/Jones_Robert_Wk6_program.py
+++ Wk6_Assignments/Jones_Robert_Wk6_program.py
@@ -12,3 +12,17 @@ def main():
     print("A story will be create with the help of you.\n")
     print("You will be prompted to enter your name, two verbs, two nouns, two adjectives, and two integers.\n")
 
+    # get user input
+    name=input("What is your name?\n")
+    verb1=input("Enter a verb ending in -ing:\n")
+    verb2=input("Enter a verb:\n")
+    plural_noun=input("Enter a plural noun:\n")
+    noun=input("Enter a noun:\n")
+    adjective1=input("Enter your first adjective:\n")
+    adjective2=input("Enter your second adjective;\n")
+    integer1=int(input("Choose a number:\n"))
+    integer2=int(input("Choose another number:\n"))
+    animal=input("Choose an animal:\n")
+    food=input("Pick a food:\n")
+    emotion=input("Pick an emotion:\n")
+
