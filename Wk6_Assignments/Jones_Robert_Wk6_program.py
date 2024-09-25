2a5aced13816c3859b5b43cd602050034a74cfb0
Robert Jones <robertdante@outlook.com>
2024-09-24 20:51:38 -0400

    introduce program

---
+++ Wk6_Assignments/Jones_Robert_Wk6_program.py
@@ -0,0 +1,14 @@
+'''
+Name: Robert Jones
+Date:9/24/24
+Purpose:The purpose of this program is to display a mad libs story involving random words from user input.
+Version:1.0
+'''
+
+# main function for user input
+def main():
+    # introduce program
+    print("Welcome to Mad Libs!\n")
+    print("A story will be create with the help of you.\n")
+    print("You will be prompted to enter your name, two verbs, two nouns, two adjectives, and two integers.\n")
+
