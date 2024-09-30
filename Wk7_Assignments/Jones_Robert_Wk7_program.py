93680a01aeb4ea56ece12408feb06edad0973126
Robert Jones <robertdante@outlook.com>
2024-09-30 12:01:32 -0400

    declare list and display menu

--- Wk7_Assignments/Jones_Robert_Wk7_program.py
+++ Wk7_Assignments/Jones_Robert_Wk7_program.py
@@ -7,6 +7,17 @@ Date: 9/30/24
 
 # main function for interactivity
 def main():
+    # declare list
+    movie = []
+    while True:
+        # Display menu options
+        print('Welcome to your Movie Recommendations!')
+        print('1. Add movie to list\n')
+        print('2. Remove movie from list\n')
+        print("3. View Programmer's recommendations \n")
+        print('4. Display list.\n')
+        print('5. Exit the program\n')
+
 
 
 if __name__ == "__main__":
