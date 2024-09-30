9c36b4361d324e923248a6c87b4cfbf4ffd8c4f8
Robert Jones <robertdante@outlook.com>
2024-09-30 12:00:18 -0400

    declare main function

---
+++ Wk7_Assignments/Jones_Robert_Wk7_program.py
@@ -0,0 +1,13 @@
+'''
+Name: Robert Jones
+Purpose: The purpose of this program is to create an interactive movie recommendations list.
+Version: 1.0
+Date: 9/30/24
+'''
+
+# main function for interactivity
+def main():
+
+
+if __name__ == "__main__":
+    main()
