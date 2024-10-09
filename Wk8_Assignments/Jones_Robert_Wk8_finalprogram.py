bef55c9b2abf6bb07c1a6862a6bb8652a3b4ffab
Robert Jones <robertdante@outlook.com>
2024-10-08 22:08:19 -0400

    functions for borrowing and returning books

--- Wk8_Assignments/Jones_Robert_Wk8_finalprogram.py
+++ Wk8_Assignments/Jones_Robert_Wk8_finalprogram.py
@@ -7,8 +7,21 @@ Date: 10/7/24
 '''
 
 # declare lists
-avail_books=['The Name of the Wind','Dune','The Girl with the Dragon Tattoo','The Nightingale','The Hating Game','The Shining','Becoming','The Hunger Games','Maus','Atomic Habits']
-borrow_books=[]
+avail_books = ['The Name of the Wind', 'Dune', 'The Girl with the Dragon Tattoo', 'The Nightingale', 'The Hating Game', 'The Shining', 'Becoming', 'The Hunger Games', 'Maus', 'Atomic Habits']
+borrow_books = []
+
+# function to remove book from avail list and adding to borrow list
+def catalog(books):
+    avail_books.remove(books)
+    borrow_books.append(books)
+    print(f'You are borrowing {books}.')
+    return books
+# function to remove book from borrow list and adding to avail list
+def borrower(books):
+    borrow_books.remove(books)
+    avail_books.append(books)
+    print(f'You have returned {books}.')
+    return books
 
 # main function for user interactivity
 def main():
@@ -28,31 +41,43 @@ def main():
 
         # decision based of user inputs
         if choice == 1:
-            # removing book from avail list and adding to borrow list
-            print(avail_books)
-            book_borrow=input("Type the name of the book you would like to borrow:\n")
-            avail_books.remove(book_borrow)
-            borrow_books.append(book_borrow)
-            print(f'You are borrowing {book_borrow}.')
+            # displaying avail book list w/o punctuation
+            while choice == 1:
+                 for book in avail_books:
+                    print(book)
+                 book_borrow=str(input("\nType the name of the book you would like to borrow:\n"))
+                 # user validation
+                 if book_borrow not in avail_books:
+                     print("Invalid entry. Please enter available title.")
+                 else:
+                    catalog(book_borrow)
+                    break
         elif choice == 2:
-            # removing book from borrow list and adding to avail list
-            print(borrow_books)
-            book_return=input('Type the name of the book that you would like to return:\n')
-            borrow_books.remove(book_return)
-            avail_books.append(book_return)
-            print(f'You have returned {book_return}.')
+            while choice == 2:
+                for book in borrow_books:
+                    print(book)
+                book_return=str(input('Type the name of the book that you would like to return:\n'))
+                # user validation
+                if book_return not in borrow_books:
+                    print('Invalid entry. Please enter correct title.')
+                else:
+                    borrower(book_return)
+                    break
         elif choice == 3:
             # displaying current state of lists
-            print(avail_books)
-            # try adding for loop to display list
+            print('Available books:')
+            for book in avail_books:
+                print(book)
             # checks if borrowed book list is empty
             if len(borrow_books) == 0:
                 print("You haven't borrowed any books yet!")
             else:
-                print(borrow_books)
+                print('\nYou are borrowing:')
+                for book in borrow_books:
+                    print(book)
         elif choice == 4:
             # quit program
-            print('You have exited the program. Goodbye!')
+            print('You have one week to return your book. Goodbye!')
             break
         else:
             # user validation
